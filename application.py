7d7f2ffa8e63dfaa307e10ebc8812135fab54ab0
Ifuwelemuel <ifuwelemuel123@gmail.com>
2024-05-27 13:30:52 +0100

    model confi

--- application.py
+++ application.py
@@ -49,5 +49,5 @@ def predict_datapoint():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    
+    app.run(host="0.0.0.0")
