3a7a0a2ef45775cfc8f48b7d7aaed5a42d93f667
Ifuwelemuel <ifuwelemuel123@gmail.com>
2024-05-27 13:26:04 +0100

    model configure

--- application.py
+++ application.py
@@ -46,6 +46,8 @@ def predict_datapoint():
         return render_template('home.html',results=results)
     
 
-if __name__ == '__main__':
-    app.run(port=8001)      
-
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
